cb6d55fb75b1f6df57b86789eb84847cca4711cb
Vishnu Vardhan Reddy <vishnureddy8980@gmail.com>
2023-04-30 16:38:27 +0300

    added image preview component

---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="relative h-48 my-4">
+		<div class="absolute top-0 left-2/4 -translate-x-2/4 rounded overflow-hidden w-48 h-48">
+			<input id="preview" type="file" @change="onFileChange" class="hidden">
+			<label for="preview" class="bg-gray-400 inline-flex w-full h-full cursor-pointer items-center justify-center">
+				<PhotoIcon class="w-6 h-6 fill-slate-500" v-if="!url" />
+				<div class="hover" v-else>
+					<span class="absolute left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4 hidden z-1 text-xs bg-gray-100 rounded p-1 text-slate-700">Change me</span>
+					<img :src="url" alt="" class="w-full h-full object-cover">
+				</div>
+			</label>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { PhotoIcon } from '@heroicons/vue/24/solid';
+import { ref } from 'vue'
+
+const url = ref(null)
+
+const onFileChange = (e) => {
+	const file =e.target.files[0];
+	url.value = URL.createObjectURL(file);
+}
+
+</script>
+
+<style lang="scss" scoped>
+.hover:hover span {
+	display: block;
+}
+</style>
--- src/components/TheResume.vue
+++ src/components/TheResume.vue
@@ -1,6 +1,14 @@
 <template>
-	<section class="flex flex-row">
-		<div class="w-1/4 px-2">test1</div>
-		<div class="w-3/4 px-2">test2</div>
+	<section class="flex flex-row text-white">
+		<div class="w-4/12 px-2 bg-slate-600">
+			<ImagePreview />
+			<h2 class="text-3xl text-center mb-2">Full Name</h2>
+			<p class="text-black text-xl text-center mb-2">Frontend Developer</p>
+		</div>
+		<div class="w-8/12 px-2">test2</div>
 	</section>
 </template>
+
+<script setup>
+import ImagePreview from '../components/ImagePreview.vue'
+</script>
