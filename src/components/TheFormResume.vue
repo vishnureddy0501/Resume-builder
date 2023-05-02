0c63caa79286a5174392df86a689ea7cc84a188f
Vishnu Vardhan Reddy <vishnureddy8980@gmail.com>
2023-05-02 09:38:07 +0300

    create constans helper js

--- src/components/TheFormResume.vue
+++ src/components/TheFormResume.vue
@@ -2,7 +2,7 @@
 	<section class="">
 		<div class="flex justify-between gap-1">
 			<button
-				v-for="btn in buttons"
+				v-for="btn in TAB_ITEMS"
 				:key="btn"
 				class="p-2 w-full uppercase font-semibold border-b-2 border-slate-600"
 				:class="{ 'pointer-events-none bg-slate-400 text-white': btn === resume.tabsStore.currentPage }"
@@ -13,15 +13,25 @@
 			>
 		</div>
 
-		<div>Content</div>
+		<div>
+			<InfoForm v-show="resume.tabsStore.currentPage === PAGE_INFO" />
+			<ExperienceForm v-show="resume.tabsStore.currentPage === PAGE_EXPERIENCE" />
+			<ProjectForm v-show="resume.tabsStore.currentPage === PAGE_PROGECT" />
+			<EducationForm v-show="resume.tabsStore.currentPage === PAGE_EDUCATION" />
+			<MoreForm v-show="resume.tabsStore.currentPage === PAGE_MORE" />
+		</div>
 	</section>
 </template>
 
 <script setup>
 import { useResumeStore } from '../stores/resume.js';
+import { TAB_ITEMS, PAGE_INFO, PAGE_EXPERIENCE, PAGE_PROGECT, PAGE_EDUCATION, PAGE_MORE  } from '../constans'
+import EducationForm from './forms/EducationForm.vue';
+import ExperienceForm from './forms/ExperienceForm.vue'
+import InfoForm from './forms/InfoForm.vue';
+import MoreForm from './forms/MoreForm.vue';
+import ProjectForm from './forms/ProjectForm.vue';
 
 const resume = useResumeStore()
-
-const buttons = ['info', 'experience', 'project', 'education', 'more']
 
 </script>
---
+++ src/constans.js
@@ -0,0 +1,7 @@
+export const PAGE_INFO = 'info'
+export const PAGE_EXPERIENCE = 'experience'
+export const PAGE_PROGECT = 'project'
+export const PAGE_EDUCATION = 'education'
+export const PAGE_MORE = 'more'
+
+export const TAB_ITEMS = [PAGE_INFO, PAGE_EXPERIENCE, PAGE_PROGECT, PAGE_EDUCATION, PAGE_MORE]
