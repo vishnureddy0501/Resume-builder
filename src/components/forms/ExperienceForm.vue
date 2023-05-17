a727efba56eef5498ba882ac14a55f8bb9c5a1eb
Vishnu Vardhan Reddy <vishnureddy8980@gmail.com>
2023-05-17 23:36:19 +0300

    not fix bug

--- src/components/forms/ExperienceForm.vue
+++ src/components/forms/ExperienceForm.vue
@@ -7,6 +7,9 @@
 				Add your role:
 			</label>
 			<input v-model="resume.experienceStore.experienceForm.role" id="role" type="text" class="px-3 py-1 border border-slate-900 rounded w-full focus:outline-none focus:shadow-border" placeholder="Your role">
+			<span v-for="error in v$.role.$errors" :key="error.$uid" class="text-red-400 text-xs leading-3">
+				{{ error.$message }}
+			</span>
 		</div>
 
 		<div class="mb-3">
@@ -15,6 +18,9 @@
 				Add company name:
 			</label>
 			<input v-model="resume.experienceStore.experienceForm.company" id="companyName" type="text" class="px-3 py-1 border border-slate-900 rounded w-full focus:outline-none focus:shadow-border" placeholder="Company name">
+			<span v-for="error in v$.company.$errors" :key="error.$uid" class="text-red-400 text-xs leading-3">
+				{{ error.$message }}
+			</span>
 		</div>
 
 		<div class="mb-3">
@@ -42,26 +48,66 @@
 				<DocumentTextIcon class="w-5 h-5" />
 				Professional Summary:
 			</label>
-			<textarea v-model="resume.experienceStore.experienceForm.description" class="px-3 py-1 border border-slate-900 rounded w-full focus:outline-none focus:shadow-border h-40 resize-none"></textarea>
+			<textarea v-model="resume.experienceStore.experienceForm.description" placeholder="Professional Summary" class="px-3 py-1 border border-slate-900 rounded w-full focus:outline-none focus:shadow-border h-40 resize-none"></textarea>
+			<span v-for="error in v$.description.$errors" :key="error.$uid" class="text-red-400 text-xs leading-3">
+				{{ error.$message }}
+			</span>
 		</div>
 
 		<div>
-			<button v-if="!resume.experienceStore.editData" class="global-btn" @click="resume.experienceStore.addExperienceData()">Add Experience</button>
-			<button v-else class="global-btn" @click="resume.experienceStore.updateExperienceData()">Edit</button>
+			<button v-if="!resume.experienceStore.editData" class="global-btn" @click="onChangeExpierence()">Add Experience</button>
+			<button v-else class="global-btn" @click="onEditExpierence()">Edit</button>
 		</div>
 
 	</div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import MainCheckboxVue from '../MainCheckbox.vue'
-import { useResumeStore } from '@/stores/resume.js';
+import { useResumeStore } from '@/stores/resume.js'
 import { BuildingOffice2Icon, UserCircleIcon, CalendarIcon, DocumentTextIcon } from '@heroicons/vue/24/outline'
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 import { enGB } from 'date-fns/locale'
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
 
 const resume = useResumeStore()
+
+// validation
+const rules = computed(() => {
+	return {
+		role: { required },
+		company: { required },
+		description: { required }
+	}
+})
+
+const v$ = useVuelidate(rules, resume.experienceStore.experienceForm)
+
+console.log(v$)
+// NEED TO FIX FUCKING BUG
+
+const onChangeExpierence = async() => {
+	const result = await v$.value.$validate()
+	if(result) {
+		resume.experienceStore.addExperienceData()
+		v$.value.$reset()
+	}
+}
+
+const onEditExpierence = async() => {
+	const result = await v$.value.$validate()
+	console.log(v$)
+	if(!result) {
+		return
+	} else {
+		resume.experienceStore.updateExperienceData()
+		v$.value.$reset()
+	}
+}
+
 
 // multi select date example
 // вдруг пригодится, хули
@@ -71,5 +117,7 @@ const resume = useResumeStore()
 //   const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
 //   date.value = [startDate, endDate];
 // })
+
+
 
 </script>
