0dab8af2886bfba28769d2c58da3f3d06c86cdd9
Vishnu Vardhan Reddy <vishnureddy8980@gmail.com>
2023-05-23 10:24:46 +0300

    add logic for download resume

--- src/components/ImagePreview.vue
+++ src/components/ImagePreview.vue
@@ -2,7 +2,7 @@
 	<div class="relative h-48 my-4">
 		<div class="absolute top-0 left-2/4 -translate-x-2/4 rounded overflow-hidden w-48 h-48">
 			<input id="preview" type="file" @change="onFileChange" class="hidden">
-			<label for="preview" class="bg-gray-400 inline-flex w-full h-full cursor-pointer items-center justify-center">
+			<label for="preview" class="bg-gray-400 flex w-full h-full cursor-pointer items-center justify-center">
 				<PhotoIcon class="w-6 h-6 fill-slate-500" v-if="!url" />
 				<div class="hover" v-else>
 					<span class="absolute left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4 hidden z-1 text-xs bg-gray-100 rounded p-1 text-slate-700">Change me</span>
--- src/components/TheResume.vue
+++ src/components/TheResume.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="flex flex-row text-white bg-white" id="resume">
-		<AsideResume class="w-4/12 px-3 bg-slate-600" />
+		<AsideResume class="w-4/12 px-3 bg-slate-600" id="resumeSide" />
 		<MainResume class="w-8/12 pl-6 pr-3 pt-8 text-slate-900" />
 		<button class="rounded-lg bg-slate-900 p-2 fixed bottom-5 right-5" @click="printResume()">
 			<DocumentArrowDownIcon class="w-7 h-7"/>
@@ -20,14 +20,40 @@ import { DocumentArrowDownIcon } from '@heroicons/vue/24/outline'
 const resume = useResumeStore()
 
 function printResume() {
-	document.getElementById('resume').setAttribute("style","width:1000px");
+	document.getElementById('resume').setAttribute("style","width:1100px");
 
 	const filename = `${resume.infoStore.infoForm.fullName} Resume.pdf`
 	const a4Width  = 210.0015555555555  // paper Standard Width
 	const a4Height = 297.0000833333333 // paper Standard Height
 	const divWidth = document.getElementById('resume').offsetWidth
 
-	console.log(filename)
+	const resumeSide = document.getElementById('resumeSide')
+
+	const resumeSideHeight = (a4Height / a4Width) * divWidth
+
+	resumeSide.setAttribute("style","min-height:" + resumeSideHeight + "px")
+
+	const resumeCV = document.getElementById('resume')
+  const divHeight = resumeCV.offsetHeight
+
+	html2canvas(resumeCV, {
+		height: divHeight,
+		width: divWidth,
+		scale: 5,
+		allowTaint: true,
+		useCORS: true,
+		logging:	false
+	}).then(canvas => {
+			let pdf = new jsPDF('p', 'mm', 'a4')
+			const width = pdf.internal.pageSize.getWidth()
+			const height = (divHeight / divWidth) * width
+			pdf.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', -2, 0, width, height)
+
+			pdf.save(filename);
+
+			resumeSide.removeAttribute("style")
+			resumeCV.removeAttribute("style")
+	});
 }
 
 </script>
