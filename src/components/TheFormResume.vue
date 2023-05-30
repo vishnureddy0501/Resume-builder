6a9762e1c28b0e6a46c677c4e7a45b542dff5ced
Vishnu Vardhan Reddy <vishnureddy8980@gmail.com>
2023-05-30 09:15:01 +0300

    fix tabs style

--- src/components/TheFormResume.vue
+++ src/components/TheFormResume.vue
@@ -1,10 +1,10 @@
 <template>
 	<section class="">
-		<div class="flex justify-between gap-1">
+		<div class="flex flex-wrap justify-between gap-1">
 			<button
 				v-for="btn in TAB_ITEMS"
 				:key="btn"
-				class="p-2 w-full uppercase font-semibold border-b-2 border-slate-600"
+				class="p-2 flex-1 uppercase font-semibold border-b-2 border-slate-600"
 				:class="{ 'pointer-events-none bg-slate-400 text-white': btn === resume.tabsStore.currentPage }"
 				@click.prevent="resume.tabsStore.navigate(btn)"
 			>
